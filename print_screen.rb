9607536ab2546c21bd51e9ea96571ea80f766f8b
Ldeems <luke.deems@yahoo.com>
2017-06-23 09:34:31 -0400

    replaced multiples of 15 with mined_minds

--- print_screen.rb
+++ print_screen.rb
@@ -1,7 +1,9 @@
 100.times do |n|
     n += 1
-    if (n % 3 == 0)
-    	puts "mined"
+    if (n % 15 == 0)
+    	puts "mined_minds"
+    elsif (n % 3 == 0)
+    	puts "mined"	
     elsif (n % 5 == 0)
     	puts "minds"	
     else puts n

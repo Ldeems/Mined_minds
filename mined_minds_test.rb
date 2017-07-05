b7c79e0b3bee4bc587574022c1ffc90dd597b3ae
Ldeems <luke.deems@yahoo.com>
2017-07-05 15:04:14 -0400

    more tests

--- mined_minds_test.rb
+++ mined_minds_test.rb
@@ -36,5 +36,16 @@ class Mined_mines_test < Minitest::Test
 	def test_mm7
 		assert_equal("mined", mm_test(33))
 	end		
-			
+	
+	def test_mm8
+		assert_equal("minds", mm_test(50))
+	end	
+
+	def test_mm9
+		assert_equal(97, mm_test(97))
+	end
+	
+	def test_mm10
+		assert_equal("mined_minds", mm_test(90))
+	end				
 end	
